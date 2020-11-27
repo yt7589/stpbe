52c4034c0e9add18d145c4451055466e75291c12
guoqiang <178224379@qq.com>
2020-11-27 18:31:08 +0800

    修改日志格式

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/MsTmdpApplication.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/MsTmdpApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
