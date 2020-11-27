0c149c1d8dd742bf095870b7b6ef80f682791b73
yantao <yt7589@qq.com>
2020-11-27 16:25:22 +0800

    set CORS in application class

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/conf/CorsConfig.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/conf/CorsConfig.java
@@ -2,13 +2,14 @@ package com.zhuanjingkj.stpbe.user.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigure;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class CorsConfig {
     @Bean
-    public WebMvcConfigure webMvcConfigure() {
-        return new WebMvcConfigure() {
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/*")
