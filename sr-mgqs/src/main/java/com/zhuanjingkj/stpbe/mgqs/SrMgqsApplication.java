5b512d228d3abe912829896fe672aa14e4269f9d
yantao <yt7589@qq.com>
2020-11-30 16:46:20 +0800

    add Milvus Graph Query sub project

---
+++ sr-mgqs/src/main/java/com/zhuanjingkj/stpbe/mgqs/SrMgqsApplication.java
@@ -0,0 +1,18 @@
+package com.zhuanjingkj.stpbe.mgqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/*@SpringBootApplication
+@EnableScheduling
+@EnableTransactionManagement(proxyTargetClass = true)
+@ComponentScan(basePackages = {"com.zhuanjingkj.stpbe.*"})*/
+public class SrMgqsApplication {
+    public static void main(String[] args) {
+        System.out.println("Mulvus Graph Query System v0.0.1");
+        //SpringApplication.run(SrMgqsApplication.class, args);
+    }
+}
---
+++ sr-mgqs/src/main/java/com/zhuanjingkj/stpbe/mgqs/mgq/MgqEngine.java
@@ -0,0 +1,12 @@
+package com.zhuanjingkj.stpbe.mgqs.mgq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MgqEngine {
+    private final static Logger logger = LoggerFactory.getLogger(MgqEngine.class);
+
+    public void demo() {
+        logger.info("Milvus Graph Query Engine Demo");
+    }
+}
