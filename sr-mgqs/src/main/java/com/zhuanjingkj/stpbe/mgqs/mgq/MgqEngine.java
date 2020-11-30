d739f429dba6b12504f0a33b249cb1ad7ac8d726
yantao <yt7589@qq.com>
2020-11-30 21:50:48 +0800

    fix the BUGs in root pom.xml to exclude guava lib

--- sr-mgqs/src/main/java/com/zhuanjingkj/stpbe/mgqs/mgq/MgqEngine.java
+++ sr-mgqs/src/main/java/com/zhuanjingkj/stpbe/mgqs/mgq/MgqEngine.java
@@ -9,9 +9,9 @@ import org.slf4j.LoggerFactory;
 public class MgqEngine {
     private final static Logger logger = LoggerFactory.getLogger(MgqEngine.class);
 
-    public void demo() {
+    public static void demo() {
         logger.info("Milvus Graph Query Engine Demo");
-        ConnectParam connectParam = new ConnectParam.Builder().withHost("127.0.0.1").withPort(19530).build();
+        ConnectParam connectParam = new ConnectParam.Builder().withHost("192.168.2.15").withPort(19530).build();
         MilvusClient client = new MilvusGrpcClient(connectParam);
         logger.info("Milvus client: " + client + "!");
     }
