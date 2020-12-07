a12f5cf8dcad913e952cc3fe33a3605e790354b5
yantao <yt7589@qq.com>
2020-12-07 18:29:37 +0800

    read the bmy id to image files dict from dataset file

--- ca-tvis/dsms/apps/dsiv/image_server.py
+++ ca-tvis/dsms/apps/dsiv/image_server.py
@@ -10,17 +10,31 @@ import urllib
 class ImageServer(object):
     def __init__(self):
         self.refl = ''
+        self.read_bmy_id_to_img_files()
+
+    def read_bmy_id_to_img_files(self):
+        num = 0
+        bmy_id_to_img_files = {}
+        with open('/media/ps/0A9AD66165F33762/yantao/dcl/datasets/'
+                    'CUB_200_2011/anno/sfds_train_ds_20201020.txt', 'r', encoding='utf-8') as fd:
+            for line in fd:
+                line = line.strip()
+                arrs = line.split('*')
+                img_file = arrs[0]
+                bmy_id = arrs[1]
+                if bmy_id not in bmy_id_to_img_files:
+                    bmy_id_to_img_files[bmy_id] = []
+                bmy_id_to_img_files[bmy_id].append(img_file)
+                num += 1
+                if num % 10000 == 0:
+                    print('已经处理{0}条记录...'.format(num))
+
+    def read_bmy_id_to_img_file_idx(self):
+        pass
 
     @staticmethod
     def display_image():
-        print('ImageServer.display_image 1')
         img_file = urllib.parse.unquote(request.args.get('img_file'))
-        print('url: {0};'.format(request.url))
-        pr = urllib.parse.urlparse(request.url)
-        qs = pr.query
-        rst_qs = urllib.parse.parse_qs(qs)
-        print('qs: {0}; {1};'.format(type(rst_qs), rst_qs))
-        print('######### {0};'.format(img_file))
         with open(img_file, 'rb') as img_fd:
             image_data = img_fd.read()
         response = flask.make_response(image_data)
@@ -33,7 +47,6 @@ CORS(app)
 
 @app.route('/displayImage', methods=['GET'])
 def display_image():
-    print('displayImage 1')
     return ImageServer.display_image()
 
 
