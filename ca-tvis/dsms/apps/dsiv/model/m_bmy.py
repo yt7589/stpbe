6b66ed51331f9ceebfe34d1d7c7c190879a4aebf
yantao <yt7589@qq.com>
2020-12-07 19:12:54 +0800

    fix BUG20201207002

--- ca-tvis/dsms/apps/dsiv/model/m_bmy.py
+++ ca-tvis/dsms/apps/dsiv/model/m_bmy.py
@@ -7,4 +7,4 @@ class MBmy(object):
     def get_bmy_ids():
         query_cond = {}
         fields = {'bmy_id': 1}
-        return MMongoDb.db['t_bmy'].find_one(query_cond, fields)
+        return MMongoDb.convert_recs(MMongoDb.db['t_bmy'].find(query_cond, fields))
