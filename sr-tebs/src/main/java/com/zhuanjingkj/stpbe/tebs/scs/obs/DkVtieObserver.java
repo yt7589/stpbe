8880d1aa2590ffab7821c38883b51c0c0d095e14
yantao <yt7589@qq.com>
2020-12-28 16:03:58 +0800

    enable TvisJsonStpListener

--- sr-tebs/src/main/java/com/zhuanjingkj/stpbe/tebs/scs/obs/DkVtieObserver.java
+++ sr-tebs/src/main/java/com/zhuanjingkj/stpbe/tebs/scs/obs/DkVtieObserver.java
@@ -3,6 +3,9 @@ package com.zhuanjingkj.stpbe.tebs.scs.obs;
 import com.zhuanjingkj.stpbe.data.vo.VehicleVo;
 import com.zhuanjingkj.stpbe.tebs.scs.ITvisStpObserver;
 
+/**
+ * 首页数据看板页面左侧第一行第一个报表，本地外埠车辆统计
+ */
 public class DkVtieObserver implements ITvisStpObserver {
     @Override
     public void notifyObserver(VehicleVo vo) {
