4cf26f23f69378eb257a2a6919fd68f1d4c60da4
yantao <yt7589@qq.com>
2020-12-28 17:08:21 +0800

    add @Component annotation to observer

--- sr-tebs/src/main/java/com/zhuanjingkj/stpbe/tebs/scs/obs/DkVtieObserver.java
+++ sr-tebs/src/main/java/com/zhuanjingkj/stpbe/tebs/scs/obs/DkVtieObserver.java
@@ -4,17 +4,23 @@ import com.zhuanjingkj.stpbe.data.vo.VehicleHptzVO;
 import com.zhuanjingkj.stpbe.data.vo.VehicleVo;
 import com.zhuanjingkj.stpbe.tebs.scs.ITvisStpObserver;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 /**
  * 首页数据看板页面左侧第一行第一个报表，本地外埠车辆统计
  */
+@Component
 public class DkVtieObserver implements ITvisStpObserver {
     @Value("${hphm.native.prefix}")
     private String hphmNativePrefix;
 
     @Override
     public void notifyObserver(VehicleVo vo) {
-        VehicleHptzVO hptzVO = vo.getVehicleHptzVO();
-        System.out.println("    ##### 调用DkVtieObserver: hphm=" + hptzVO.getHphm() + "! vs " + hphmNativePrefix + "!!!!!!!!!!!!");
+        String hphm = vo.getVehicleHptzVO().getHphm();
+        if (hphm.indexOf(hphmNativePrefix) >= 0) {
+            System.out.println("    ##### 本埠车辆：" + hphm + "!");
+        } else {
+            System.out.println("    ##### 外地车辆：" + hphm + "!");
+        }
     }
 }
