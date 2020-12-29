09cc23f358c127d99e877d0bd441bcf102a0522c
yantao <yt7589@qq.com>
2020-12-29 20:13:50 +0800

    fix BUG20201229005

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/impl/DkVtieService.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/impl/DkVtieService.java
@@ -17,8 +17,8 @@ public class DkVtieService implements IDkVtieService {
     @Override
     public DkVtieDTO getDkVtie() {
         DkVtieDTO data = new DkVtieDTO();
-        long ti = (Long)redisTemplate.opsForValue().get("dkInternalNum");
-        long te = (Long)redisTemplate.opsForValue().get("dkExternalNum");
+        int ti = (Integer)redisTemplate.opsForValue().get("dkInternalNum");
+        int te = (Integer)redisTemplate.opsForValue().get("dkExternalNum");
         data.setInternalPercent((int)(ti/(ti+te+0.001)*100));
         data.setExternalPercent((int)(te/(ti+te+0.001)*100));
         return data;
