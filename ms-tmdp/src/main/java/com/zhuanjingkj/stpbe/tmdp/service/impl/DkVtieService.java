8cbc62c9861f79dcc2a593b213d93c2ea9c58265
yantao <yt7589@qq.com>
2020-12-29 18:29:12 +0800

    call redis increment with 0 increase

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/impl/DkVtieService.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/impl/DkVtieService.java
@@ -16,9 +16,9 @@ public class DkVtieService implements IDkVtieService {
     @Override
     public DkVtieDTO getDkVtie() {
         DkVtieDTO data = new DkVtieDTO();
-        System.out.println("######## incr=" + redisTemplate.opsForValue().get("dkInternalNum") + "!");
-        long ti = Long.parseLong((String)redisTemplate.opsForValue().get("dkInternalNum"));
-        long te = Long.parseLong((String)redisTemplate.opsForValue().get("dkExternalNum"));
+        System.out.println("######## incr=" + redisTemplate.opsForValue().increment("dkInternalNum", 0) + "!");
+        long ti = redisTemplate.opsForValue().increment("dkInternalNum", 0);
+        long te = redisTemplate.opsForValue().increment("dkExternalNum", 0);
         System.out.println("########## ti=" + ti + "; te=" + te + "!");
         data.setInternalPercent((int)(ti/(ti+te+0.001)));
         data.setExternalPercent((int)(te/(ti+te+0.001)));
