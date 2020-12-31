48a291f6e0a9bd534f4ff354754a080ef82e2bc6
yantao <yt7589@qq.com>
2020-12-31 14:53:38 +0800

    change /tn/va/queryDevice response JSON in camel naming convention

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/dto/tn/TnDeviceDTO.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/dto/tn/TnDeviceDTO.java
@@ -6,11 +6,11 @@ import com.zhuanjingkj.stpbe.data.dto.BaseDTO;
 public class TnDeviceDTO extends BaseDTO {
     @JSONField(name = "camera")
     private Integer camera;
-    @JSONField(name = "abnormal_camera")
+    @JSONField(name = "abnormalCamera")
     private Integer abnormal_camera;
     @JSONField(name = "snapshot")
     private Integer snapshot;
-    @JSONField(name = "abnormal_snapshot")
+    @JSONField(name = "abnormalSnapshot")
     private Integer abnormal_snapshot;
 
     public TnDeviceDTO(Integer camera, Integer abnormal_camera, Integer snapshot, Integer abnormal_snapshot) {
