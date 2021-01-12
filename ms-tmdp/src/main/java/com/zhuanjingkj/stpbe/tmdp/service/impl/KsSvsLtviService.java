a19079aa8a3e33bc1471d7ec6850a42bdc351d17
fph <13052510071@163.com>
2021-01-12 19:34:45 +0800

    特殊车辆监管地图图片修改

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/impl/KsSvsLtviService.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/impl/KsSvsLtviService.java
@@ -21,7 +21,8 @@ public class KsSvsLtviService implements IKsSvsLtviService {
     public KsSvsLtviDTO getKsSvsLtviDTO_exp() {
         String tvisJsonTblName = tvisJsonMapper.getLatesTvisJsonTblName();
         KsSvsLtviDTO ksSvsLtviDTO = ksSvsLtviMapper.getKsSvsLtvi(tvisJsonTblName);
-        ksSvsLtviDTO.setImageUrl(IpfsClient.getIpfsUrl(ksSvsLtviDTO.getImageHash()));
+        //ksSvsLtviDTO.setImageUrl(IpfsClient.getIpfsUrl(ksSvsLtviDTO.getImageHash()));
+        ksSvsLtviDTO.setImageUrl("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1606650551241&di=8378d72dc6414bfa9a243c2e75db511a&imgtype=0&src=http%3A%2F%2Fimg1.gtimg.com%2Fauto%2Fpics%2Fhv1%2F246%2F190%2F1582%2F102918246.jpg");
         return ksSvsLtviDTO;
     }
 }
