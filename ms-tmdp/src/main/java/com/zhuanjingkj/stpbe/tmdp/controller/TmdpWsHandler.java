13832b74e64baa0d23ee52cb787bb7a546891ab1
yantao <yt7589@qq.com>
2021-01-20 20:50:42 +0800

    generate video analysis json to browser

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/controller/TmdpWsHandler.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/controller/TmdpWsHandler.java
@@ -3,6 +3,7 @@ package com.zhuanjingkj.stpbe.tmdp.controller;
 import com.zhuanjingkj.stpbe.tmdp.service.impl.VideoAnalysisService;
 import com.zhuanjingkj.stpbe.tmdp.task.VideoAnalysisTask;
 import org.apache.commons.lang.StringUtils;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -43,7 +44,7 @@ public class TmdpWsHandler extends TextWebSocketHandler {
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message)
-            throws InterruptedException, IOException {
+            throws InterruptedException, IOException, JSONException {
         String payload = message.getPayload();
         JSONObject jsonObject = new JSONObject(payload);
         String user = jsonObject.getString("userId");
