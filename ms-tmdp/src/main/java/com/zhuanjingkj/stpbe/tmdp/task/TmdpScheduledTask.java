2b43def14fbcc9db03504d85cfcfee0a5e39996f
yantao <yt7589@qq.com>
2021-01-26 13:46:16 +0800

    remove debug log in TmdpScheduledTask

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/task/TmdpScheduledTask.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/task/TmdpScheduledTask.java
@@ -161,9 +161,6 @@ public class TmdpScheduledTask {
                     String hphm = val.split("\\|")[0];
                     String cameraId = val.split("\\|")[1];
                     String coordinate = "" + KsRssService.rssSiteMap.get(cameraId);
-                    System.out.println("coordinate:" + coordinate);
-                    System.out.println(coordinate.split("\\|")[0]);
-                    System.out.println(coordinate.split("\\|")[1]);
                     if(StringUtils.isNotBlank(coordinate)) {
                         KsRssSfvsDTO lsv = new KsRssSfvsDTO(101, "" + KsRssService.rssMap.get(cameraId),
                                 hphm, Integer.parseInt("" + redisTemplate.opsForHash().get("ks_rss_lsvs_total", val)),
