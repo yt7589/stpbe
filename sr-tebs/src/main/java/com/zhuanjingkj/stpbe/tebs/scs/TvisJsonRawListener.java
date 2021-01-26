0b6d80158706c8d9759bfdbee49b7e349b721a49
yantao <yt7589@qq.com>
2021-01-26 11:32:21 +0800

    delete kafka topic content

--- sr-tebs/src/main/java/com/zhuanjingkj/stpbe/tebs/scs/TvisJsonRawListener.java
+++ sr-tebs/src/main/java/com/zhuanjingkj/stpbe/tebs/scs/TvisJsonRawListener.java
@@ -50,6 +50,11 @@ public class TvisJsonRawListener {
 
     @KafkaListener(id = "TvisJsonRawListener", topics = "tvis")
     public void listen(String json) {
+        System.out.println("delete topic content: " + json + "!");
+        int iDebug = 1;
+        if (1 == iDebug) {
+            return;
+        }
         logger.info("step 1");
         if (!isInitialized) {
             synchronized (logger) {
