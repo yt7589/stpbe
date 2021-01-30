6aba3d9beb00addc76d98402f9eaa078db379bf1
yantao <yt7589@qq.com>
2021-01-30 18:27:31 +0800

    fix BUG20210130008

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/impl/TmdpService.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/impl/TmdpService.java
@@ -8,7 +8,9 @@ import com.zhuanjingkj.stpbe.tmdp.service.ITmdpService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TmdpService implements ITmdpService {
     private final static String LIST_VEHICLE_RECOGNITION = "vehicle-recognition-list";
     private final static Logger logger = LoggerFactory.getLogger(TmdpService.class);
