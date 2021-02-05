6a433dd7327979edc7152fd9ee4f8aead12e785d
yantao <yt7589@qq.com>
2021-02-05 15:35:24 +0800

    move sjzx=>ytsc to DcCsService

---
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/IDcCsService.java
@@ -0,0 +1,10 @@
+package com.zhuanjingkj.stpbe.tmdp.service;
+
+import com.zhuanjingkj.stpbe.data.dto.DbQrsDTO;
+import com.zhuanjingkj.stpbe.data.dto.ResultDTO;
+
+public interface IDcCsService {
+    public ResultDTO<DbQrsDTO> queryVehicleByGraph(String cltzxl, String psfx, String cllxfl,
+                                                   String cllxzfl, String startDate, String endDate,
+                                                   String startTime, String endTime);
+}
