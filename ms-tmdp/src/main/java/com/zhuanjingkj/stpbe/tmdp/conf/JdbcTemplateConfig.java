fef05003d0122ff8068be44fb8482c936550f775
yantao <yt7589@qq.com>
2021-02-05 19:09:32 +0800

    remove JdbcTemplateConfig logic from ms-tmdp

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/conf/JdbcTemplateConfig.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/conf/JdbcTemplateConfig.java
@@ -1,13 +1,8 @@
 package com.zhuanjingkj.stpbe.tmdp.conf;
 
-import com.zhuanjingkj.stpbe.db.DataSourceRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JdbcTemplateConfig {
-    @Bean
-    public DataSourceRegistry dataSourceRegistry() {
-        return DataSourceRegistry.getInstance();
-    }
 }
