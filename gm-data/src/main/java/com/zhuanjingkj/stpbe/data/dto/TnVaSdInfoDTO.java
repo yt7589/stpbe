d9a0b26355d769960e5b7c13a155d09d54d916c9
yantao <yt7589@qq.com>
2021-02-22 19:56:32 +0800

    fix BUGs in get camera stream_id 002

--- gm-data/src/main/java/com/zhuanjingkj/stpbe/data/dto/TnVaSdInfoDTO.java
+++ gm-data/src/main/java/com/zhuanjingkj/stpbe/data/dto/TnVaSdInfoDTO.java
@@ -11,15 +11,15 @@ public class TnVaSdInfoDTO extends BaseDTO {
     @JSONField(name = "diAddr")
     private String diAddr; //设备位置
     @JSONField(name = "direction")
-    private String direction; //设备方向
+    private int direction; //设备方向
     @JSONField(name = "category")
-    private String category; //设备类别
+    private int category; //设备类别
     @JSONField(name = "status")
-    private String status; //设备状态
+    private int status; //设备状态
     @JSONField(name = "streamId")
     private long streamId;
 
-    public TnVaSdInfoDTO(long diId, String diAddr, String direction, String category, String status, long streamId) {
+    public TnVaSdInfoDTO(long diId, String diAddr, int direction, int category, int status, long streamId) {
         this.diId = diId;
         this.diAddr = diAddr;
         this.direction = direction;
@@ -44,27 +44,27 @@ public class TnVaSdInfoDTO extends BaseDTO {
         this.diAddr = diAddr;
     }
 
-    public String getDirection() {
+    public int getDirection() {
         return direction;
     }
 
-    public void setDirection(String direction) {
+    public void setDirection(int direction) {
         this.direction = direction;
     }
 
-    public String getCategory() {
+    public int getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(int category) {
         this.category = category;
     }
 
-    public String getStatus() {
+    public int getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(int status) {
         this.status = status;
     }
 
