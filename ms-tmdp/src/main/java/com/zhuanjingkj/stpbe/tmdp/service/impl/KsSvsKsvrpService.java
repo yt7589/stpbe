1fc4c6228dc8b1cabd6ac2105c907ddabff0c88c
fph <13052510071@163.com>
2021-02-23 21:55:58 +0800

    特殊车辆底部4图代码修改

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/impl/KsSvsKsvrpService.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/service/impl/KsSvsKsvrpService.java
@@ -19,10 +19,10 @@ public class KsSvsKsvrpService implements IKsSvsKsvrpService {
     @Override
     public List<KsSvsKsvrpDTO> getKsSvsKsvrpDTOs_exp() {
         List<KsSvsKsvrpDTO> list = new ArrayList<>();
-//        list.add(new KsSvsKsvrpDTO(1,  IpfsClient.getIpfsUrl("" + redisTemplate.opsForList().leftPop("ks_ksvrp_images"))));
-//        list.add(new KsSvsKsvrpDTO(2, IpfsClient.getIpfsUrl("" + redisTemplate.opsForList().leftPop("ks_ksvrp_images"))));
-        list.add(new KsSvsKsvrpDTO(1,  "http://222.128.117.234:9003/imgs/zdjgsscl1.png"));
-        list.add(new KsSvsKsvrpDTO(2,  "http://222.128.117.234:9003/imgs/zdjgsscl2.png"));
+        list.add(new KsSvsKsvrpDTO(1,  IpfsClient.getIpfsUrl("" + redisTemplate.opsForList().leftPop("ks_ksvrp_images"))));
+        list.add(new KsSvsKsvrpDTO(2, IpfsClient.getIpfsUrl("" + redisTemplate.opsForList().leftPop("ks_ksvrp_images"))));
+//        list.add(new KsSvsKsvrpDTO(1,  "http://222.128.117.234:9003/imgs/zdjgsscl1.png"));
+//        list.add(new KsSvsKsvrpDTO(2,  "http://222.128.117.234:9003/imgs/zdjgsscl2.png"));
         return list;
     }
 }
