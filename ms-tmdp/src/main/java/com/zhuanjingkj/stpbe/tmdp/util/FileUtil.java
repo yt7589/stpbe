59cd3a918f730f5e6c2ddd452a35fad93d074d8c
fph <13052510071@163.com>
2021-02-27 15:41:57 +0800

    首页违章统计sql修改

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/util/FileUtil.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/util/FileUtil.java
@@ -20,102 +20,55 @@ import java.lang.reflect.Method;
 public class FileUtil {
 
     public static void export(HttpServletResponse response, FileExpDTO fileExp) {
-
         HSSFWorkbook workbook = new HSSFWorkbook();
-
         HSSFSheet sheet = workbook.createSheet(fileExp.getTitle());
-
         HSSFRow row = sheet.createRow(0);
-
         HSSFCell cell = row.createCell(0);
-
         CellRangeAddress cellRangeAddress = new CellRangeAddress(0,0,0,fileExp.getColumns().length);
-
         sheet.addMergedRegion(cellRangeAddress);
-
         HSSFCellStyle cellStyle = workbook.createCellStyle();
-
         cellStyle.setAlignment(HorizontalAlignment.CENTER);
-
         cell.setCellStyle(cellStyle);
-
         cell.setCellValue(fileExp.getFileName());
-
         HSSFDataFormat dataFormat = workbook.createDataFormat();
-
         short format = dataFormat.getFormat("yyyy-MM-dd");
-
         HSSFCellStyle dataStyle = workbook.createCellStyle();
-
         dataStyle.setDataFormat(format);
-
         sheet.setColumnWidth(4, 20 * 256);
-
         HSSFCellStyle fontStyle = workbook.createCellStyle();
-
         fontStyle.setAlignment(HorizontalAlignment.CENTER);
-
         fontStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-
         HSSFFont font = workbook.createFont();
-
         font.setBold(true);
-
         font.setColor(Font.COLOR_RED);
-
         font.setItalic(true);
-
         font.setFontName("楷体");
-
         font.setFontHeightInPoints((short)10);
-
         fontStyle.setFont(font);
-
         HSSFRow row1 = sheet.createRow(1);
-
         for (int i = 0; i < fileExp.getColumns().length; i++) {
-
             HSSFCell cell1 = row1.createCell(i);
-
             cell1.setCellStyle(fontStyle);
-
             cell1.setCellValue(fileExp.getColumns()[i]);
         }
-
         for (int i = 0; i < fileExp.getList().size(); i++) {
-
             Object obj = fileExp.getList().get(i);
-
             Class clss = obj.getClass();
-
             Field[] fields = obj.getClass().getDeclaredFields();
-
             int column = 0;
-
             HSSFRow rowi = sheet.createRow(i + 2);
-
             for (Field field : fields) {
-
                 HSSFCell cell1 = rowi.createCell(column);
-
                 if ("serialVersionUID".equals(field.getName())) {
-
                     continue;
-
                 }
                 try {
                     PropertyDescriptor pd = new PropertyDescriptor(field.getName(), clss);
-
                     Method getMethod = pd.getReadMethod();
-
                     Object o1 = getMethod.invoke(obj);
-
                     System.out.println("字段名称："+field.getName() + " 字段值： "+o1);
-
                     cell1.setCellStyle(dataStyle);
-
                     cell1.setCellValue(o1 + "");
-
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
