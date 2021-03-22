f2f14359231ec58652be6df498fa8bb220180f27
fph <13052510071@163.com>
2021-03-22 10:47:34 +0800

    修改配置路径为68路径

--- gm-common/src/main/java/com/zhuanjingkj/stpbe/common/AppConst.java
+++ gm-common/src/main/java/com/zhuanjingkj/stpbe/common/AppConst.java
@@ -39,8 +39,8 @@ public class AppConst {
     // IPFS相关
     public final static String IPFS_API_URL = "http://192.168.2.82:5005/api/v0/"; // 加命令 add, cat等
     public final static String IPFS_GW_URL = "http://192.168.2.82:9095/ipfs/"; // 加文件hash
-//    public final static String VIDEO_FRAME_IMG_BASE_DIR = "/home/ps/yantao/stp/dev/car_analyzer/";
-    public final static String VIDEO_FRAME_IMG_BASE_DIR = "/home/jhc/";
+    public final static String VIDEO_FRAME_IMG_BASE_DIR = "/home/ps/yantao/stp/dev/car_analyzer/";
+//    public final static String VIDEO_FRAME_IMG_BASE_DIR = "/home/jhc/";
     public final static String JSON_TMP_BASE_DIR = "/tmp/";
     //
     public final static String TVIS_JSON_TBL_PREFIX = "StpDb.t_tvis_json_";
