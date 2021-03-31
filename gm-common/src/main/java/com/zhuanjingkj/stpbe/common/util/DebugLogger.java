bb1183210bd787e312d2ae774e67a8374cc0e843
yantao <yt7589@qq.com>
2021-03-31 18:13:35 +0800

    调整调试日志工具类不是每次都关闭文件

--- gm-common/src/main/java/com/zhuanjingkj/stpbe/common/util/DebugLogger.java
+++ gm-common/src/main/java/com/zhuanjingkj/stpbe/common/util/DebugLogger.java
@@ -4,39 +4,31 @@ import java.io.*;
 
 public class DebugLogger {
     public static String LOG_FILE = "/home/ps/yantao/stp/logs/yt.log";
+    private static FileOutputStream fos = null;
+    private static OutputStreamWriter osw = null;
+
     public static void log(String msg) {
         log(LOG_FILE, msg);
     }
 
     public static void log(String logFile, String msg) {
-        FileOutputStream fos = null;
-        OutputStreamWriter osw = null;
+        if (null == fos) {
+            try {
+                fos = new FileOutputStream(logFile, true); // 以追加方式打开文件
+                osw = new OutputStreamWriter(fos, "UTF-8");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
         try {
-            fos = new FileOutputStream(logFile, true); // 以追加方式打开文件
-            osw = new OutputStreamWriter(fos, "UTF-8");
+
             osw.write(msg);
             osw.flush();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (osw != null) {
-                try {
-                    osw.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
