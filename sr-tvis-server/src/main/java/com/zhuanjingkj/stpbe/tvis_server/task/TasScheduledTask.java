3e401ebc0036031cf4b9f50a0fa8f806cd54157a
yantao <yt7589@qq.com>
2021-04-01 15:14:17 +0800

    t002

--- sr-tvis-server/src/main/java/com/zhuanjingkj/stpbe/tvis_server/task/TasScheduledTask.java
+++ sr-tvis-server/src/main/java/com/zhuanjingkj/stpbe/tvis_server/task/TasScheduledTask.java
@@ -89,7 +89,9 @@ public class TasScheduledTask implements Runnable {
                 + tvisJsonId + ", \"json\": " + response + "}");
         //}
         String json = msg.toString();
+        DebugLogger.log("######################### yt: runTasScheduledTask 4");
         TvisUtil.processRawTvisJson(redisTemplate, tvisJsonMapper, json);
+        DebugLogger.log("######################### yt: runTasScheduledTask 5");
         if (isFirstRun) {
             tvisStpOberverManager.initialize(observers, environment);
             Thread thd = null;
@@ -100,7 +102,9 @@ public class TasScheduledTask implements Runnable {
             }
             isFirstRun = false;
         }
+        DebugLogger.log("######################### yt: runTasScheduledTask 6");
         TvisUtil.processStpTvisJson(observers, json);
+        DebugLogger.log("######################### yt: runTasScheduledTask 7");
         /*
         // 从Redis中读出视频识别结果，将其发送到Kafka
         // 向Kafka的Topic发送请求
