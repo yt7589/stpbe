76cd6fba2e09ba00cc0dc33800cd161e3597fa1c
fph <13052510071@163.com>
2021-04-18 15:37:51 +0800

    访问路径

--- ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/controller/ZjcBillController.java
+++ ms-tmdp/src/main/java/com/zhuanjingkj/stpbe/tmdp/controller/ZjcBillController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
  *个人账单
  */
 @RestController
-@RequestMapping(value = "")
+@RequestMapping(value = "/zjc/bill/")
 @CrossOrigin(value = "*")
 public class ZjcBillController {
 
